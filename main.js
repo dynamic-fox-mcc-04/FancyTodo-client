30986de638b578eb2fc5462e29d79ecb304f2ba1
hariszaki17 <hariszaki17@gmail.com>
2020-04-01 23:35:22 +0700

    front-end baru belum masuk ke server

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const baseUrl = 'http://localhost:3000'
 
 $( document ).ready(function() {
-    $( '.main-page' ).hide()
-    $( '.register' ).hide()
-    $( '.login' ).show()
+    $( '.main-page' ).show()
+    auth()
     $( '#login-form' ).submit(login(event))
+    $( '#register-form' ).submit(register(event))
 })
+
+const register = (event) => {
+    event.preventDefault()
+    const email = $( '#email-register' ).val()
+    const password = $( '#password-register' ).val()
+    $.ajax({
+        method: 'POST',
+        url: baseUrl + '/users/signup',
+        data: {
+            email,
+            password
+        }
+    })
+    .done(data => {
+        localStorage.setItem('access_token', data.accessToken)
+        auth()
+    })
+    .fail(err => {
+        err.responseJSON.errors.forEach(data => {
+            console.log(data.message)
+        })
+        auth()
+    })
+}
 
 const login = (event) => {
     event.preventDefault()
@@ -21,23 +45,37 @@ const login = (event) => {
     })
     .done(data => {
         localStorage.setItem('access_token', data.accessToken)
+        auth()
         console.log(data)
     })
     .fail(err => {
         err.responseJSON.errors.forEach(data => {
             console.log(data.message)
         })
+        auth()
     })
     
 }
 
-const toRegister = () => {
+const auth = () => {
+    if  (localStorage.access_token) {
+        $( '.main-page' ).show()
+        $( '.register' ).hide()
+        $( '.login' ).hide()
+    } else {
+        $( '.main-page' ).hide()
+        $( '.register' ).hide()
+        $( '.login' ).show()
+    }
+}
+
+const toRegister = (event) => {
     event.preventDefault()
     $( '.login' ).hide()
     $( '.register' ).show()
 }
 
-const toLogin = () => {
+const toLogin = (event) => {
     event.preventDefault()
     $( '.login' ).show()
     $( '.register' ).hide()
