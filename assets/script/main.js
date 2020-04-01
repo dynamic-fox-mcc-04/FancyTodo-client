12ca4cab96c1877409b900502493e7d2a1cd56cc
RaihanArdianata <RaihanArdianata@users.noreply.github.com>
2020-04-02 01:45:14 +0700

    done -in status  not yet

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,30 +2,97 @@ let baseUrl = 'http://localhost:3000'
 
 $(document).ready(() => {
     // alert('welcome')
+    $('#add-todo').click(function () {
+        $('#modal-create , #bg').fadeIn("slow");
+    });
+    $('#tombol-tutup').click(function () {
+
+        $('#modal-create , #bg').fadeOut("slow");
+    });
+
+    $('#tombol-tutup-update').click(function () {
+        $('#modal-update , #bg').fadeOut("slow");
+    });
     auth()
 })
 
-function signout(){
+function reportCorona(){
+    $.ajax({
+        method: 'GET',
+        url: baseUrl + '/api/getCovidReportAll',
+        headers: {
+            token: localStorage.token
+        }
+    })
+        .done(data => {
+            $('#dataCorona').empty()
+            console.log(data);
+            const cases = data.data.cases
+            const recovered = data.data.recovered
+            const death = data.data.deaths
+            $('#dataCorona').append(
+               `<div class="column" style="background-color: rgb(223, 100, 0);color: whitesmoke;">
+                    <p class="bd-notification is-info">Cases</p>
+                    <label>${cases}</label>
+                    <br>
+                    <br>
+                    <div class="columns is-mobile">
+                        <div class="column" style="background-color: rgb(230, 0, 0);color: whitesmoke;">
+                        <p class="bd-notification is-info">Deaths</p>
+                        <label>${death}</label>
+                        </div>
+                        <div class="column" style="background-color: green;color: whitesmoke;">
+                        <p class="bd-notification is-info">Recovered</p>
+                        <label>${recovered}</label>
+                        </div>
+                    </div>
+                </div>`
+            )
+        })
+        .fail(data => {
+            console.log(data)
+        })
+}
+
+function signout() {
+    $('#bg').fadeIn("slow");
+    $('#loading').attr('style', 'display:none !important');
     localStorage.clear()
+    $('#bg').fadeOut("slow");
     auth()
 }
 
 function auth() {
-    if(localStorage.token){
-        $('#Form-Login').hide()
-        $('#Todo-List').show()
+    if (localStorage.token) {
+        $('#Form-Login').attr('style', 'display:none !important');
+        $('#Form-SignUp').attr('style', 'display:none !important');
+        $('#Todo-List').attr('style', 'display:grid !important');
         fetchData()
-    }else{
-        $('#Form-Login').show()
-        $('#Todo-List').hide()
+        reportCorona()
+    } else {
+        $('#Form-Login').attr('style', 'display:table !important ; margin-top:100px !important');
+        $('#Todo-List').attr('style', 'display:none !important');
     }
 }
 
+function modalDaftar(){
+    $('#Form-SignUp').attr('style', 'display:table !important; margin-top: 100px !important;');
+    $('#Form-Login').attr('style', 'display:none !important');
+}
+
+function modalMasuk(){
+    $('#Form-SignUp').attr('style', 'display:none !important; margin-top: 100px !important;');
+    $('#Form-Login').attr('style', 'display:table !important; margin-top: 100px !important;');
+}
+
 function signup(event) {
+    $('#bg').fadeIn("slow");
+    $('#loading').attr('style', 'display:table !important; display: flex !important;justify-content: center !important; align-items: center !important;');
     event.preventDefault()
-    const email = $('#email').val()
-    const password = $('#password').val()
-    console.log(email,password)
+    const email = $('#email-daftar').val()
+    const password = $('#password-daftar').val()
+    console.log(email, password);
+    
     $.ajax({
         method: 'POST',
         url: baseUrl + '/signup',
@@ -35,20 +102,24 @@ function signup(event) {
         }
     })
         .done(data => {
-            console.log(data)
+            $('#loading').attr('style', 'display:none !important');
+            $('#bg').fadeOut("slow");
             localStorage.setItem('token', data.token)
             auth()
         })
         .fail(err => {
+            $('#loading').attr('style', 'display:none !important');
+            $('#bg').fadeOut("slow");
             console.log(err)
         })
 }
 
 function signin(event) {
     event.preventDefault()
+    $('#bg').fadeIn("slow");
+    $('#loading').attr('style', 'display:table !important; display: flex !important;justify-content: center !important; align-items: center !important;');
     const email = $('#email').val()
     const password = $('#password').val()
-    console.log(email, password)
     $.ajax({
         method: 'POST',
         url: baseUrl + '/signin',
@@ -58,58 +129,167 @@ function signin(event) {
         }
     })
         .done(data => {
-            console.log(data)
+            $('#loading').attr('style', 'display:none !important');
+            $('#bg').fadeOut("slow");
             localStorage.setItem('token', data.token)
             auth()
         })
         .fail(err => {
+            $('#loading').attr('style', 'display:none !important');
+            $('#bg').fadeOut("slow");
             console.log(err)
         })
 }
 
 function fetchData() {
     $.ajax({
-        method:'GET',
+        method: 'GET',
         url: baseUrl + '/todo',
-        headers:{
+        headers: {
             token: localStorage.token
         }
     })
-    .done(data =>{
-        for(let i in data){
-            const title = data[i].title
-            const description = data[i].description
-            const status = data[i].status
-            $('#Todo-list'),append(
-                `<div class="card" style="width:70%">
-                    <header class="header-container">
-                        <h3>John Doe</h3>
-                    </header>
-                    <div class="body-container">
-                        <p>1 new friend request</p>
-                        <hr>
-                        <img src="img_avatar3.png" alt="Avatar" class="w3-left w3-circle w3-margin-right"
-                            style="width:60px">
-                        <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p>
-                        <br>
+        .done(data => {
+            $('.data-item').empty()
+            console.log(data);
+            for (let i in data) {
+                const id = data[i].id
+                const title = data[i].title
+                const description = data[i].description
+                const date = new Date(data[i].due_date).toDateString()
+                const status = data[i].status
+                const email = data[i].User.email
+
+                $('.data-item').append(
+                    `
+                <div class="column is-half">
+                    <div class="box"  >
+                        <div style="display: flex;justify-content: flex-end;">
+                            <a onclick="deleteTodo(${id})" class="delete"></a>
+                        </div>
+                        <article class="media" style="cursor:pointer;" onclick="findById(${id})">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                <p>
+                                    <strong>${title}</strong> <small>${email}</small> <small>${date}</small>
+                                    <br>
+                                    ${description}
+                                </p>
+                                </div>
+                            </div>
+                        </article>
                     </div>
-                    <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
-                </div>`
-            )
+                </div>
+                `
+                )
+            }
+        })
+        .fail(data => {
+            console.log(data)
+        })
+}
+
+function createTodo(event) {
+    event.preventDefault()
+    const title = $('#title').val()
+    const description = $('#description').val()
+    const due_date = $('#due-date').val()
+
+    $.ajax({
+        method: 'POST',
+        url: baseUrl + '/todo',
+        data: {
+            title,
+            description,
+            due_date
+        },
+        headers: {
+            token: localStorage.token
         }
     })
-    .fail(data =>{
-        console.log(data)
+        .done(data => {
+            auth()
+            $('#modal-create , #bg').fadeOut("slow");
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
+function findById(id) {
+    $.ajax({
+        method: 'GET',
+        url: baseUrl + '/todo/' + id,
+        headers: {
+            token: localStorage.token
+        }
     })
+        .done(data => {
+
+            let date = new Date(data.due_date)
+            let month = date.getMonth() + 1
+            let day = date.getDate()
+            if (month < 10) {
+                month = '0' + month
+            }
+            if (day < 10) {
+                day = '0' + day
+            }
+
+            const newDate = date.getFullYear() + "-" + month + "-" + day
+            $('#id-update').val(data.id)
+            $('#title-update').val(data.title)
+            $('#description-update').val(data.description)
+            $('#due-date-update').val(newDate)
+            $('#modal-update , #bg').fadeIn("slow");
+        })
+        .fail(err => {
+            console.log(err)
+        })
 }
 
-function createTodo() {
-
+function updateTodo(event) {
+    event.preventDefault()
+    const title = $('#title-update').val()
+    const description = $('#description-update').val()
+    const due_date = $('#due-date-update').val()
+    $.ajax({
+        method: 'PUT',
+        url: baseUrl + '/todo/' + $('#id-update').val(),
+        headers: {
+            token: localStorage.token
+        }, data: {
+            title,
+            description,
+            due_date
+        }
+    })
+        .done(data => {
+            auth()
+            $('#modal-update , #bg').fadeOut("slow");
+        })
+        .fail(err => {
+            console.log(err)
+        })
 }
 
-function updateTodo() {
-
-}
-
-function deleteTodo() {
+function deleteTodo(id) {
+    $.ajax({
+        method: 'DELETE',
+        url: baseUrl + '/todo/' + id,
+        headers: {
+            token: localStorage.token
+        }
+    })
+        .done(data => {
+            auth()
+        })
+        .fail(err => {
+            console.log(err)
+        })
 }
