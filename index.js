09cb8823a67e06b35edfb92897a5252c62fd1da0
Yoko <baghram0001@gmail.com>
2020-04-02 23:39:16 +0700

    Updated to Login Register Select Project Done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const BaseUrl = 'http://localhost:3000'
+
 $(document).ready(function() {
-    if(localStorage.getItem('AccessToken')) {
+    if(localStorage.getItem('Access_Token')) {
         StartSignedIn()
     }
     else {
@@ -24,6 +25,10 @@ $(document).ready(function() {
     $('#Register').on('click', function() {
         Register()
     })
+    //TOMBOL LOGOUT
+    $('#Logout').on('click', function() {
+        Logout()
+    })
     //REGISTER PROCEDURE
     $("#FormRegister").on('submit', function(e) {
         console.log(BaseUrl+'/user/register')
@@ -39,12 +44,78 @@ $(document).ready(function() {
         .done(function(result) {
             console.log(result)
             swal("Good job!", result.message, "success");
+            RegisterClear()
 
         })
         .fail(function(err){
             swal("Too Bad", err.responseJSON.message, "error");
+            RegisterClear()
+
         })
     }) 
     //LOGIN PROCEDURE
-    
+    $('#FormLogin').on('submit', function(e) {
+        e.preventDefault()
+        console.log($('#EmailLogin').val())
+        console.log($('#PasswordLogin').val())
+        $.ajax({
+            url: BaseUrl+'/user/login',
+            method: 'POST',
+            data: {
+                Email: $('#EmailLogin').val(),
+                Password: $('#PasswordLogin').val()
+            }
+        })
+            .done(function(result) {
+                localStorage.setItem('Access_Token', result.Access_Token)
+                swal('Login Success', `You Are Accessing From IP ${result.geolocation.ip}, Location ${result.geolocation.city}`, 'success')
+                SignedIn()
+                LoginClear()
+            })
+            .fail(function(err) {
+                swal('Login Failed', err.responseJSON.message, 'error')
+                LoginClear()
+            })
+        
+    })
+    //CREATE PROJECT PROCEDURE
+    $('#AddProject').on('submit', function(e) {
+        e.preventDefault()
+        console.log($('#ProjectName').val())
+        $.ajax({
+            url: BaseUrl+'/projects/add',
+            method: 'POST',
+            headers: {
+                Access_Token: localStorage.getItem('Access_Token')
+            },
+            data: {
+                Title: $('#ProjectName').val()
+            }
+        })
+            .done(function(result) {
+                swal('Add Project Success', `Success Creating Project`, 'success')
+                console.log(result)
+                GenerateProject()
+                RegisterClear()
+            })
+            .fail(function(err) {
+                console.log(err)
+                swal('Add Project Failed', err.responseJSON.message, 'error')
+            })
+    })
+    //SELECT PROJECT (GENERATE TODO)
+    $('#SelectProject').on('submit', function(e) {
+        e.preventDefault()
+        console.log($('#Project').val())
+
+    })
+
+    //CREATE TODO PROCEDURE
+    $('#TodoCreate').on('submit', function(e) {
+        e.preventDefault()
+        console.log($('#CreateTitle').val())
+        console.log($('#CreateContent').val())
+        console.log($('#CreateDate').val())
+        console.log($('#CreateStatus').val())
+    })
 })
--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ function StartNotSignedIn() {
     $('#Logout').hide()
     $('#LoginForm').hide()
     $('#RegisterForm').hide()
+    $('#ProjectBody').hide()
     $('#MainBody').hide()
 
 }
@@ -11,9 +12,11 @@ function StartSignedIn() {
     $('#Register').hide()
     $('#LoginForm').hide()
     $('#RegisterForm').hide()
-    $('#MainBody').show()
-
+    $('#MainBody').hide()
+    $('#ProjectBody').show()
+    GenerateProject()
 }
+
 
 function Login() {
     //Button
@@ -23,6 +26,7 @@ function Login() {
     //Body
     $('#LoginForm').show()
     $('#RegisterForm').hide()
+    $('#ProjectBody').hide()
     $('#MainBody').hide()
 
 }
@@ -35,6 +39,100 @@ function Register() {
     //Body
     $('#LoginForm').hide()
     $('#RegisterForm').show()
+    $('#ProjectBody').hide()
     $('#MainBody').hide()
 }
 
+function Logout() {
+    //Button
+    $('#Login').show()
+    $('#Register').show()
+    $('#Logout').hide()
+    //Body
+    $('#LoginForm').hide()
+    $('#RegisterForm').hide()
+    $('#ProjectBody').hide()
+    $('#MainBody').hide()
+    localStorage.removeItem('Access_Token')
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    });
+    //empty reset
+    FormClear()
+    
+
+}
+
+function LoginClear() {
+    $("#EmailLogin").val("")
+    $("#PasswordLogin").val("")
+}
+
+function RegisterClear() {
+    $("#EmailRegister").val("")
+    $("#PasswordRegister").val("")
+}
+
+function FormClear() {
+    $("#Project").empty()
+    $("#ProjectName").val("")
+    $("#Members").empty()
+    $("#MemberEmail").val("")
+    
+}
+
+function TodoClear() {
+    $("#CreateTitle").val("")
+    $("#CreateContent").val("")
+    $("#CreateDate").val("")
+}
+
+function UpdateClear() {
+    $("#UpdateTitle").val("")
+    $("#UpdateContent").val("")
+    $("#UpdateDate").val("")
+}
+
+function SignedIn() {
+    //Button
+    $('#Login').hide()
+    $('#Register').hide()
+    $('#Logout').show()
+    //Body
+    $('#LoginForm').hide()
+    $('#RegisterForm').hide()
+    $('#ProjectBody').show()
+    $('#MainBody').hide()
+    GenerateProject()
+}
+
+function GenerateProject() {
+    $.ajax({
+        url: BaseUrl+'/projects/',
+        method: "GET",
+        headers: {
+            Access_Token: localStorage.getItem('Access_Token')
+        }
+    })
+        .done(function(result) {
+            for(let i = 0; i < result.length; i++) {
+                $('#Project').append(`
+                <option value='${result[i].Project.id}'>${result[i].Project.Title}</option>
+                
+                `
+                )
+
+            }
+            console.log(result)
+
+        })
+        .fail(function(err) {
+            console.log(err)
+        })
+}
+
+//Google Login
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log('testing google sign in')
+}
