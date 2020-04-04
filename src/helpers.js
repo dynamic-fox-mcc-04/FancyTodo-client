4bfbeef3496573661dcbe36a8104317b4375b267
maxhamz <maxwell.hamzah@gmail.com>
2020-04-04 11:47:21 +0700

    deployed

---
+++ src/helpers.js
@@ -0,0 +1,26 @@
+function showLogin() {
+    $('.lds-dual-ring').hide()
+
+    $('.login-page').show()
+    $('.main-page').hide()
+    $('.todos-page').hide()
+    $('.invitation-page').hide()
+    $('.showmembers-page').hide()
+    $('.update-page').hide()
+
+}
+
+
+function showMain() {
+    $('.lds-dual-ring').hide()
+
+    $('.login-page').hide()
+    $('.main-page').show()
+    $('.todos-page').hide()
+    $('.invitation-page').hide()
+    $('.showmembers-page').hide()
+    $('.update-page').hide()
+
+}
+
+
