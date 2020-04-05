9dbaeded6c458de027a947dbca8dfd729cda3956
thomasmartuaps <thomasmartuaps@gmail.com>
2020-04-05 18:47:09 +0700

    error updating

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ function showRegister(event) {
     $('#dashboard').hide()
 }
 function showDashboard() {
-    console.log('masuk dashboard')
     $('#username').empty()
     $('#pic').empty()
     $('#username').append(`${localStorage.getItem('email')}`)
@@ -24,9 +23,46 @@ function showDashboard() {
     getTasks()
 }
 
+function addTask(event) {
+    event.preventDefault()
+    $.ajax({
+        method: 'POST',
+        url: `${localhost}/todos`,
+        headers: {
+            token: localStorage.getItem('token')
+        },
+        data: {
+            title: $('#title-add').val(),
+            description: $('#description-add').val(),
+            due_date: $('#date-add').val()
+        }
+    }).done(response => {
+        console.log(response, 'added')
+        $('#title-add').val('')
+        $('#description-add').val('')
+        $('#date-add').val('')
+        getTasks()
+    }).fail(err => {
+        console.log(err, "adding task error")
+        err.responseJSON.forEach(el => {
+            $('#error-message').append(`${el}<br>`)
+            $('#alert').fadeTo(2000, 500).slideUp(500, function() {
+                $("#alert").slideUp(500);
+                $('#error-message').empty()
+            })
+        })
+    })
+}
+
+function complete(event, arr) {
+    event.preventDefault()
+    console.log(arr)
+}
+
 function getTasks() {
     console.log('dapat task')
     $('#tasks-list').empty()
+    $('#loading').show()
     $.ajax({
         method: 'GET',
         url: `${localhost}/todos`,
@@ -37,24 +73,14 @@ function getTasks() {
         console.log(response, 'ini todo nya')
         response.forEach(el => {
             let completed = ''
-            if(el.status === false) {
-                completed = `<span class="tag is-pulled-right is-danger">Not Done</span>`
-            } else {
-                completed = `<span class="tag is-pulled-right is-success">Done</span>`
-            }
             let created = new Date(el.createdAt)
             let due = new Date(el.due_date)
-            $('#tasks-list').append(`
+            let toAppend = `
             <div class="card">
   <header class="card-header">
     <p class="card-header-title">
       ${el.title}  ${completed}
     </p>
-    <a href="#" class="card-header-icon" aria-label="more options">
-      <span class="icon">
-        <i class="fas fa-angle-down" aria-hidden="false"></i>
-      </span>
-    </a>
   </header>
   <div class="card-content">
     <div class="content">
@@ -66,13 +92,22 @@ function getTasks() {
     </div>
   </div>
   <footer class="card-footer">
-  <a href="" onclick="complete(event)" class="card-footer-item">Complete</a>
-    <a href="" onclick="showUpdate(event)" class="card-footer-item">Edit</a>
-    <a href="" onclick="delete(event)" class="card-footer-item">Delete</a>
+    <button onclick="complete(event, [${el.id}, ${el.title}, ${el.description}, ${el.due_date}])" class="button card-footer-item">Complete</button>
+    <button class="button card-footer-item">Edit</button>
+    <button class="button card-footer-item">Delete</button>
   </footer>
 </div>
-            `)
+            `
+            if(el.status === false) {
+                completed = `<span class="tag status is-pulled-right is-danger">Not Done</span>`
+                $('#tasks-list').append(toAppend)
+            } else {
+                completed = `<span class="tag status is-pulled-right is-success">Done</span>`
+                $('#tasks-list-done').append(toAppend)
+            }
+            
         })
+        $('#loading').hide(4000)
     })
     .fail(err => {
             console.log(err)
@@ -162,6 +197,7 @@ function logout(event) {
 $(document).ready(function() {
     showLandingPage()
     $('#alert').hide()
+    $('#loading').hide()
     let token = localStorage.getItem('token')
     if(!token) {
         showLandingPage()
